522d17183bb3050db808bbe2c19ca5646035aeab
ronardj01 <ronardj01@gmail.com>
2021-06-17 18:46:17 +0200

    Inserted 5 mentors to table mentors

--- week-1/mandatory/cyf_classes.sql
+++ week-1/mandatory/cyf_classes.sql
@@ -6,3 +6,17 @@ CREATE TABLE mentors (
     favorite_programming_language       VARCHAR(30) NOT NULL
 );
 
+INSERT INTO mentors (name, years_in_Glasgow, address, favorite_programming_language) 
+VALUES ('Miguel Erja Gonzalez Solis', 2, '4, Shepford Place', 'HTML');
+
+INSERT INTO mentors (name, years_in_Glasgow, address, favorite_programming_language) 
+VALUES ('Nandan Rao', 4, '16 John Wood St', 'CSS');
+
+INSERT INTO mentors (name, years_in_Glasgow, address, favorite_programming_language)
+VALUES ('Alexandra Yamaui', 2, '10 Cromdale Rd', 'Javascript');
+
+INSERT INTO mentors (name, years_in_Glasgow, address, favorite_programming_language)
+VALUES ('Linoman', 1, '14 Stroma Avenue', 'Javascript');
+
+INSERT INTO mentors (name, years_in_Glasgow, address, favorite_programming_language)
+VALUES ('Alvaro Perez', 6, '28 Dubbs Rd', 'ReactJS');
