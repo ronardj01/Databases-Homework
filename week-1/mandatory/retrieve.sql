126782438e05e2d8808d162fba33f7677ab45a7a
ronardj01 <ronardj01@gmail.com>
2021-06-18 09:17:40 +0200

    Added retieve.sql file

---
+++ week-1/mandatory/retrieve.sql
@@ -0,0 +1,7 @@
+SELECT * FROM mentors WHERE years_in_Glasgow > 5;
+
+SELECT * From mentors WHERE favorite_programming_language = 'Javascript';
+
+SELECT * FROM students WHERE graduated = TRUE;
+
+SELECT * FROM classes WHERE classes_date < '2021-06-01';
