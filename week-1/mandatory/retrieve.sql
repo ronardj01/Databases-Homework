1b8aa1352e826116f1de9d824f9214f127dabe10
ronardj01 <ronardj01@gmail.com>
2021-06-19 16:34:22 +0200

    Added select query to retieve.sql

--- week-1/mandatory/retrieve.sql
+++ week-1/mandatory/retrieve.sql
@@ -5,3 +5,7 @@ SELECT * From mentors WHERE favorite_programming_language = 'Javascript';
 SELECT * FROM students WHERE graduated = TRUE;
 
 SELECT * FROM classes WHERE classes_date < '2021-06-01';
+
+select * from attendees;
+
+select topic from classes where id = 2;
