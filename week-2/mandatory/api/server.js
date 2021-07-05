613f979346968a255de8b065f6f976966afef305
ronardj01 <ronardj01@gmail.com>
2021-07-05 10:54:05 +0200

    Added enpoint to load all supliers

--- week-2/mandatory/api/server.js
+++ week-2/mandatory/api/server.js
@@ -5,6 +5,7 @@ const { Pool } = require('pg');
 //queries
 
 const getAllCustomers = 'select * from customers c';
+const getAllSupliers = 'select * from suppliers s';
 
 const pool = new Pool ({
   user: 'migracode',
@@ -20,6 +21,15 @@ app.get('/customers', function (req, res) {
     res.json(result.rows)
   });
 });
+
+app.get('/supliers', function (req, res) {
+  pool.query(getAllSupliers, (err, result) => {
+    console.log(result)
+    res.json(result.rows)
+  });
+});
+
+
 
 
 app.listen(3000, function () {
