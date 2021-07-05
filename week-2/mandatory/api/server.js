2b20b6f515859ed23cdba91543c66ddc52eedcba
ronardj01 <ronardj01@gmail.com>
2021-07-05 10:51:29 +0200

    Added an enpoint to load all customers

--- week-2/mandatory/api/server.js
+++ week-2/mandatory/api/server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
+const { Pool } = require('pg');
 
+//queries
+
+const getAllCustomers = 'select * from customers c';
+
+const pool = new Pool ({
+  user: 'migracode',
+  host: 'localhost',
+  database: 'cyf_ecommerce',
+  password: '',
+  port: 5432
+});
+
+app.get('/customers', function (req, res) {
+  pool.query(getAllCustomers, (err, result) => {
+    console.log(result)
+    res.json(result.rows)
+  });
+});
 
 
 app.listen(3000, function () {
