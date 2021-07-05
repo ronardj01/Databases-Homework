f90fb0c91032b616db0293502024df1a621402d5
ronardj01 <ronardj01@gmail.com>
2021-07-05 11:05:11 +0200

    Added enpoint to load products name without supliers

--- week-2/mandatory/api/server.js
+++ week-2/mandatory/api/server.js
@@ -3,11 +3,12 @@ const app = express();
 const { Pool } = require('pg');
 
 //queries
-
 const getAllCustomers = 'select * from customers c';
 const getAllSupliers = 'select * from suppliers s';
+const getAllProductsAndSupliers =  "select p.product_name, s.supplier_name from products p inner join suppliers s on p.supplier_id = s.id";
 
-const pool = new Pool ({
+//connection setting
+const pool = new Pool({
   user: 'migracode',
   host: 'localhost',
   database: 'cyf_ecommerce',
@@ -15,22 +16,24 @@ const pool = new Pool ({
   port: 5432
 });
 
+//enpoints
 app.get('/customers', function (req, res) {
   pool.query(getAllCustomers, (err, result) => {
-    console.log(result)
     res.json(result.rows)
   });
 });
 
 app.get('/supliers', function (req, res) {
   pool.query(getAllSupliers, (err, result) => {
-    console.log(result)
     res.json(result.rows)
   });
 });
 
-
-
+app.get('/products', function (req, res) {
+  pool.query(getAllProductsAndSupliers, (err, result) => {
+    res.json(result.rows)
+  });
+});
 
 app.listen(3000, function () {
   console.log('Server listening on port 3000')
