21472040ffe4cca11beeff84a000ec2fc53bd4e1
ronardj01 <ronardj01@gmail.com>
2021-07-10 09:20:52 +0200

    Added a new endpoint to load customer by id

--- week-3/mandatory/api/router/customers.js
+++ week-3/mandatory/api/router/customers.js
@@ -3,14 +3,30 @@ const router = express.Router();
 
 //Queries
 const getAllCustomers = 'select * from customers c';
+const customerByID = 'select * from customers c where id = $1';
 
 //Connecting to the Database
 const pool = require('../utils/poolConect');
 
+//Endpoints
+//Search customer by Id
+router.get('/:customerID', async function (req, res) {
+  const customerID = req.params.customerID;
+  try{
+    const result = await pool.query(customerByID, [customerID]);
+    res.json(result.rows);
+  }catch(error) {
+    console.error(error.stack)
+  }
+});
+
+//Search all customer
 router.get('/', function (req, res) {
     pool.query(getAllCustomers, (err, result) => {
       res.json(result.rows)
     });
   });
+
+
 
 module.exports = router;
